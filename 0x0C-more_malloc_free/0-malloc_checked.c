e61cb1c48d92c438fe71fb4c9f197f3036700ee0
raheldaniel14 <raheldaniel41@gail.com>
2022-04-07 20:56:17 +0100

    0-malloc_checked.c

---
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,37 @@
+int **alloc_grid(int width, int height)
+{
+ int **array;
+ int i = 0;
+ int j = 0;
+ int k = width;
+ int p = height;
+
+ if (width <= 0 || height <= 0)
+ {
+  return (NULL);
+ }
+ array = (int **)malloc(sizeof(int *) * height);
+ if (array == NULL)
+ {
+  return (NULL);
+ }
+ while (i < p)
+ {
+  array[i] = malloc(sizeof(int) * width);
+  if (array[i] == NULL)
+  {
+   for (i--; i >= 0; i--)
+    free(array[i]);
+   free(array);
+   return (NULL);
+  }
+  while (j < k)
+  {
+   array[i][j] = 0;
+   j++;
+  }
+  i++;
+ }
+ return (array);
+}
+
